e6bb2ca08096bab33b74afc01a13988b497340c1
nisarkhan <nisarkhan@hotmail.com>
2015-03-14 21:07:16 -0400

    remove body parser from index.js

--- index.js
+++ index.js
@@ -4,12 +4,12 @@ var express = require('express');
 var app = express();
 //var mongojs = require('mongojs');
 //var db = mongojs('contactlist', ['contactlist']);
-var bodyParser = require('body-parser');
+//var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
  
 
-app.use(bodyParser.json()); 
+//app.use(bodyParser.json()); 
 
 var port = process.env.PORT || 3030;
 app.listen(port);
